bcd766e9c64c9a30407401b9730895b6b29b084b
CaptainHammered <75399892+CaptainHammered@users.noreply.github.com>
2022-01-08 14:58:21 -0600

    fix

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -1,47 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Mail;
 using MailKit.Net.Smtp;
-using NETcore.MailKit.Core;
+using MailKit;
+using MimeKit;
+using Microsoft.Extensions.Configuration;
 
 namespace WillardSoftware.Pages
 {
     public class ContactModel : PageModel
     {
         private readonly ILogger<ContactModel> _logger;
-        private readonly IEmailService _emailService;
+        
+        #nullable enable
         public ContactModel(ILogger<ContactModel> logger)
         {
             _logger = logger;
         }
-
+        #nullable disable
         public void OnGet()
         {
         }
 
-
+        
 
         [BindProperty]
         public ContactForm Contact { get; set; }
 
-        public void Send(ContactForm Contact)
-        {
-            MimeMessage message = new MimeMessage();
-        }
+        
+
+
 
     }
 
     public class ContactForm
     {
-    [Required]
-    public string Name { get; set; } = default!;
-    [Required]
-    public string LastName { get; set; } = default!;
-    [Required]
-    public string Email { get; set; } = default!;
-    [Required]
-    public string Message { get; set; } = default!;
+        [Required]
+        public string Name { get; set; } = default!;
+        [Required]
+        public string LastName { get; set; } = default!;
+        [Required]
+        public string Email { get; set; } = default!;
+        [Required]
+        public string Message { get; set; } = default!;
     }
 }
