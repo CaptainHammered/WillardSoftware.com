2c64c1ded637f4aa37fe25ed6bbd99ca143d1fc4
CaptainHammered <75399892+CaptainHammered@users.noreply.github.com>
2022-01-08 14:44:54 -0600

    test

--- Pages/Contact.cshtml.cs
+++ Pages/Contact.cshtml.cs
@@ -1,79 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using System.ComponentModel.DataAnnotations;
 using System.Net.Mail;
 using MailKit.Net.Smtp;
-using MailKit;
-using MimeKit;
+using NETcore.MailKit.Core;
 
-namespace WillardSoftware.Pages;
-
-public class ContactModel : PageModel
+namespace WillardSoftware.Pages
 {
-    private readonly ILogger<ContactModel> _logger;
-
-    public ContactModel(ILogger<ContactModel> logger)
+    public class ContactModel : PageModel
     {
-        _logger = logger;
-    }
-
-    public void OnGet()
-    {
-    }
-
-    [BindProperty]
-    public ContactFormModel Contact { get; set; }
-
-    public async Task<IActionResult> OnPostAsync()
-    {
-        if (!ModelState.IsValid)
+        private readonly ILogger<ContactModel> _logger;
+        private readonly IEmailService _emailService;
+        public ContactModel(ILogger<ContactModel> logger)
         {
-            return Page();
+            _logger = logger;
         }
 
-        // create and send the mail here
-        var mailbody = $@"Hi Willard,
-
-        Someone is trying to reach you from your website!
-
-        Name: {Contact.Name}
-        LastName: {Contact.LastName}
-        Email: {Contact.Email}
-        Message: ""{Contact.Message}""
-        ";
-
-        SendMail(mailbody);
-        
-
-        return RedirectToPage("Index");
-    }
-    private void SendMail(string mailbody)
-    {
-        using (var message = new MailMessage(Contact.Email, "willard@willardsoftware.com"))
+        public void OnGet()
         {
-            message.To.Add(new MailAddress("willard@willardsoftware.com"));
-            message.From = new MailAddress(Contact.Email);
-            message.Subject = "New E-Mail from my website";
-            message.Body = mailbody;        
-            
-            SmtpClient client = new SmtpClient("smtp-mail.outlook.com");
-            client.EnableSsl = true;
-            client.UseDefaultCredentials = true;
-            client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-            client.Send(message);
+        }
+
+
+
+        [BindProperty]
+        public ContactForm Contact { get; set; }
+
+        public void Send(ContactForm Contact)
+        {
+            MimeMessage message = new MimeMessage();
         }
 
     }
-}
 
-public class ContactFormModel
-{
-  [Required]
-  public string Name { get; set; } = default!;
-  [Required]
-  public string LastName { get; set; } = default!;
-  [Required]
-  public string Email { get; set; } = default!;
-  [Required]
-  public string Message { get; set; } = default!;
+    public class ContactForm
+    {
+    [Required]
+    public string Name { get; set; } = default!;
+    [Required]
+    public string LastName { get; set; } = default!;
+    [Required]
+    public string Email { get; set; } = default!;
+    [Required]
+    public string Message { get; set; } = default!;
+    }
 }
